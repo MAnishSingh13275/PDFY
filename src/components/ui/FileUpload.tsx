023c5d8d5e6d1edf0388ca3c5643dd13da5f3896
Manish Singh <manishsingh13275@gmail.com>
2023-11-04 21:38:26 +0530

    Integrated AWS

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -1,15 +1,26 @@
 "use client";
+import { uploadToS3 } from "@/lib/s3";
 import { Inbox } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 
-
 const FileUpload = () => {
   const { getRootProps, getInputProps } = useDropzone({
-    accept: {"application/pdf": [".pdf"]},
+    accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
-    onDrop: (acceptedFiles) => {
+    onDrop: async (acceptedFiles) => {
       console.log(acceptedFiles);
+      const file = acceptedFiles[0];
+      if (file.size > 10 * 1024 * 1024) {
+        alert("File size must be less than 10MB");
+        return;
+      }
+      try {
+        const data = await uploadToS3(file);
+        console.log("data", data);
+      } catch (error) {
+        console.log(error);
+      }
     },
   });
 
@@ -17,13 +28,14 @@ const FileUpload = () => {
     <div className="bg-white rounded-xl p-2">
       <div
         {...getRootProps({
-          className: "border-dashed border-2 border-gray-400 rounded-xl cursor-pointer p-2 py-10 ",
+          className:
+            "border-dashed border-2 border-gray-400 rounded-xl cursor-pointer p-2 py-10 ",
         })}
       >
         <input {...getInputProps()} />
         <>
-        <Inbox size={34} className="mx-auto" />
-        <p className="text-center font-bold">PDFy your PDF</p>
+          <Inbox size={34} className="mx-auto" />
+          <p className="text-center font-bold">PDFy your PDF</p>
         </>
       </div>
     </div>
