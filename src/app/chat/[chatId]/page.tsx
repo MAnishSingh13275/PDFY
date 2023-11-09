858d2070473d63e81f63ad49c7de3fd75c5d07a6
Manish Singh <manishsingh13275@gmail.com>
2023-11-09 22:45:22 +0530

    Intitializing Chats

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -37,7 +37,7 @@ const page = async ({ params: { chatId } }: Props) => {
           <PDFViewer pdf_url={currentChat?.pdfUrl || ""} />
         </div>
 
-        <div className="flex-[3] border-l-4 border-slate-300 "> </div>
+        <div className="flex-[3] border-l-4 border-slate-300 "> Chats </div>
       </div>
     </div>
   );
